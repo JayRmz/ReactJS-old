055301477257ae29421437ba64c7ad4d7159b7f6
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2021-08-28 08:43:18 -0500

    Day 1

---
+++ BasicConcepts/mi-primera-app/src/components/AdiosMundo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AdiosMundo (){
+    return (
+        <>
+            <h3>
+                Bye bye!
+            </h3>
+        </>
+    );
+}
---
+++ BasicConcepts/mi-primera-app/src/components/HolaMundo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+export default function HolaMundo() {
+
+    const miFuncion = () => {
+        console.log("Mi función");
+    };
+
+    return (
+    <div>
+        <h1>Hola Mundo</h1>
+        <h2>Jay Ramírez</h2>
+    </div>
+    );
+}
