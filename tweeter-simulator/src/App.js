4256c0a49a53b0d2e6385ba7fcda09e868a22f09
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2021-11-07 10:53:04 -0600

    Tweet simulator done

--- tweeter-simulator/src/App.js
+++ tweeter-simulator/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import { Container, Snackbar } from "@mui/material";
-import SendTweet from "./components/SendTweet/SendTweet";
+import SendTweet from "./components/SendTweet";
 import { TWEETS_STORAGE } from "./utils/constants";
+import ListTweets from "./components/ListTweets";
 function App() {
   const [toastProps, setToastProps] = useState({
     open: false,
@@ -10,18 +11,28 @@ function App() {
   });
 
   const [allTweets, setAllTweets] = useState([]);
+  const [reloadTweets, setRealoadTweets] = useState(false);
 
   useEffect(() => {
     const allTweetsStorage = localStorage.getItem(TWEETS_STORAGE);
     const allTweetsArray = JSON.parse(allTweetsStorage);
     setAllTweets(allTweetsArray);
-  }, []);
+    setRealoadTweets(false);
+  }, [reloadTweets]);
+
+  const deleteTweet = (index) => {
+    allTweets.splice(index, 1);
+    setAllTweets(allTweets);
+    localStorage.setItem(TWEETS_STORAGE, JSON.stringify(allTweets));
+    setRealoadTweets(true);
+  };
 
   console.log(allTweets);
   return (
     <Container className="tweets-simulator" maxWidth={false}>
       <Header />
       <SendTweet setToastProps={setToastProps} allTweets={allTweets} />
+      <ListTweets allTweets={allTweets} deleteTweet={deleteTweet} />
       <Snackbar
         anchorOrigin={{ vertical: "top", horizontal: "right" }}
         open={toastProps.open}
